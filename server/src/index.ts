4d3d8b239a0dec196015287bbbea8d0547e8ea81
Daniel Ceron <desceron@unicauca.edu.co>
2025-01-26 18:47:33 -0500

    remove bun and add node support

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import { Server } from 'socket.io';
-import dummyStockData from './data/dummy_stock_data.json';
+import dummyStockData from '../data/dummy_stock_data.json';
 
 const StockData = dummyStockData;
 
